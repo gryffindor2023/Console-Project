f9be970b1eece82e6abff782ec3a330bf30c488a
gryffindor2023 <150682454+gryffindor2023@users.noreply.github.com>
2024-01-12 00:10:59 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+var first_name = "Ranbir";
+console.log(first_name);
+console.log("first name="+first_name);
+
+var last_name = " Kapoor";
+console.log(last_name);
+console.log("last name="+last_name);
+
+var a = 10;
+var b = 5;
+var concatName = first_name.concat(last_name);
+console.log(concatName);
+
+var add = a + b;
+console.log(add);
